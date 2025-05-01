2fe7c25bcd840e4af1230195e6ad055f0bada555
Aakash-Srinivasan <aakash4367595@gmail.com>
2025-05-01 23:34:22 +0530

    env added

--- supabase.ts
+++ supabase.ts
@@ -1,7 +1,7 @@
 // lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://ikfbiqfjtmwhxacydeuj.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrZmJpcWZqdG13aHhhY3lkZXVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4Njc1MzgsImV4cCI6MjA2MDQ0MzUzOH0.fCJdEgJR9n9RYtDkW0wLYwFTtave93ZqkdtG5xOLVx8';
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPA_BASE_URL;
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPA_BASE_PUBLIC_ANNON_KEY;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL as any, SUPABASE_ANON_KEY as any);
